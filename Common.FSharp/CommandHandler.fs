a82399ea917b40b0bc515aa36ee9efe14e0dd6a2
Phillip Scott Givens <psgivens11@yahoo.com>
2019-05-18 18:11:57 +0000

    Adding 'empty' command handler to account for non-eventing commands.

--- Common.FSharp/CommandHandler.fs
+++ Common.FSharp/CommandHandler.fs
@@ -10,19 +10,24 @@ type CommandHandlerBuilder<'event, 'state> (apply:'state -> 'event -> 'state) =
             let state' = apply state event              
             return! rest () state'
         }
-    member this.Return (result:Async<'event>) state = 
+    member this.Return (result:Async<Option<'event>>) state = 
         async { 
             let! event = result
-            return apply state event
+            return 
+                match event with
+                | None -> state
+                | Some evt -> apply state evt
         }
 
 [<RequireQualifiedAccess>]
 module Handler =
-    let Raise event = async { return event }
+    let Raise event = async { return Some(event) }
+    let Empty () = async { return None }
     let Run initialState handler = handler initialState
 
 type CommandHandlers<'event,'state> (raiseVersionedEvent:'state -> 'event -> 'state) =
     member this.block = CommandHandlerBuilder raiseVersionedEvent
     member this.event event = this.block { return event |> Handler.Raise }
+    member this.empty state = async { return state }
     
 
